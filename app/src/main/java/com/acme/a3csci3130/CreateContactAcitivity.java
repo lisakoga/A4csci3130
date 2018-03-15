e7a4ddbb0f3fc8d1e1524960f4aecb89497d5eb8
lizzie <ls256469@dal.ca>
2018-03-15 00:11:29 -0300

    updated

--- app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+/**
+ * CreateContactActivity.java file
+ * This program creates a new contact information and adds the new contact to the database
+ */
 
 public class CreateContactAcitivity extends Activity {
 
     private Button submitButton;
-    private EditText nameField, emailField;
+    private EditText nameField, numberField, businessField, addressField, provinceField;
     private MyApplicationData appState;
 
     @Override
@@ -21,15 +25,21 @@ public class CreateContactAcitivity extends Activity {
 
         submitButton = (Button) findViewById(R.id.submitButton);
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        numberField = (EditText) findViewById(R.id.number);
+        businessField = (EditText) findViewById(R.id.business);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceField = (EditText) findViewById(R.id.province);
     }
 
     public void submitInfoButton(View v) {
         //each entry needs a unique ID
         String personID = appState.firebaseReference.push().getKey();
         String name = nameField.getText().toString();
-        String email = emailField.getText().toString();
-        Contact person = new Contact(personID, name, email);
+        String number = numberField.getText().toString();
+        String business = businessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+        Contact person = new Contact(personID, name, number, business, address, province);
 
         appState.firebaseReference.child(personID).setValue(person);
 
--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -5,33 +5,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Intent;
 
-public class DetailViewActivity extends Activity {
+/**
+ * DetailViewActivity.java
+ * This program allows users to edit, update, or delete their personal information
+ */
+public class DetailViewActivity extends Activity
+{
 
-    private EditText nameField, emailField;
+    private EditText nameField, numberField, businessField, addressField, provinceField;
     Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        numberField = (EditText) findViewById(R.id.number);
+        businessField = (EditText) findViewById(R.id.business);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceField = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            numberField.setText(receivedPersonInfo.number);
+            businessField.setText(receivedPersonInfo.business);
+            addressField.setText(receivedPersonInfo.address);
+            provinceField.setText(receivedPersonInfo.province);
         }
     }
 
-    public void updateContact(View v){
-        //TODO: Update contact funcionality
+    public void updateContact(View v)
+    {
+        String name = nameField.getText().toString();
+        String number = numberField.getText().toString();
+        String business = businessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+
+        // updates the firebase
+        Contact updated = new Contact(receivedPersonInfo.uid, name, number, business, address, province);
+        appState.firebaseReference.child(receivedPersonInfo.uid).setValue(updated);
+        finish();
+
     }
 
     public void eraseContact(View v)
     {
-        //TODO: Erase contact functionality
+        // erase the contact from the firebase
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        finish();
     }
 }
